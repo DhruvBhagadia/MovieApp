bcad80b0b5c0b001009f128b0b5df92c7579d31e
DhruvBhagadia <dhruvbhagadia@gmail.com>
2017-12-01 09:27:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell_pc/movieapp/adapter/MoviesAdapter.java
@@ -0,0 +1,8 @@
+package com.example.dell_pc.movieapp.adapter;
+
+/**
+ * Created by DELL_PC on 30-11-2017.
+ */
+
+public class MoviesAdapter {
+}
---
+++ app/src/main/java/com/example/dell_pc/movieapp/rest/ApiInterface.java
@@ -0,0 +1,8 @@
+package com.example.dell_pc.movieapp.rest;
+
+/**
+ * Created by DELL_PC on 30-11-2017.
+ */
+
+public interface ApiInterface {
+}
