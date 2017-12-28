61050a26e160bc734c2effc15f39e250c3d21336
DhruvBhagadia <dhruvbhagadia@gmail.com>
2017-12-28 15:49:44 +0530

    First commit in the branch

---
+++ app/src/main/java/com/example/dell_pc/movieapp/Fragments/TwoFragment.java
@@ -0,0 +1,32 @@
+package com.example.dell_pc.movieapp.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dell_pc.movieapp.R;
+
+/**
+ * Created by DELL_PC on 08-12-2017.
+ */
+
+public class TwoFragment extends Fragment{
+
+    public TwoFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
